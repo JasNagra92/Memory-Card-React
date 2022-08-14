7a0142318d935d90fde1656a9333eaf0ad16ab85
jasmac <jaskarannagra@hotmail.com>
2022-08-14 03:47:54 -0700

    write function that when given an index number, loops through the array in state and flips the cooresponding index to true

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,28 @@
-import React, {useState} from 'react';
-
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [Cards, setCards] = useState({
-    cards: Array(12).fill(false)
-  })
+    cards: Array(11).fill(false),
+  });
+  const [score, setScore] = useState(0);
+  const handleClick = (i) => {
+      setCards({cards: Cards.cards.map((card, index) => {
+        if (index === i) {
+          return card = true
+        } else {return card}
+      })})
+      setScore(score + 1)
+    }
 
-  
-  
+  useEffect(()=>{
+    console.log(Cards)
+  }, [Cards])
+
   return (
     <div className="App">
-      <div>
-
-      </div>
+      <button onClick={()=>handleClick(3)}>test</button>
     </div>
   );
-}
+};
 
 export default App;
