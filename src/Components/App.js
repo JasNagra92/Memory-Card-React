7f1eff7b5e3b0a1436c0a3269c23e1a0a3ed95bb
jasmac <jaskarannagra@hotmail.com>
2022-08-14 03:25:46 -0700

    App function with state variables for keeping score and the 12 cards that will be rendered

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,17 @@ import React, {useState} from 'react';
 
 
 const App = () => {
+  const [Cards, setCards] = useState({
+    cards: Array(12).fill(false)
+  })
 
+  
   
   return (
     <div className="App">
+      <div>
 
+      </div>
     </div>
   );
 }
